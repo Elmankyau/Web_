4a3474e1256f611829fb80de09c5ae8f50cefe5f
Elmankyau <alamelman@gmail.com>
2023-01-28 21:13:50 +0600

    v 1.2

--- data.php
+++ data.php
@@ -1,8 +1,21 @@
-<html>
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "project2";
 
-<body>
+    //create a connection
+    
+    $conn = mysqli_connect($servername, $username, $password, $database);
 
-    <?php
+    //connection status
+    if (!$conn) {
+        die("Disconnect" . mysqli_connect_error());
+    } else {
+        echo "Connected";
+        echo "<br>";
+    }
+
     $name = $_POST["name"];
     $username = $_POST["uname"];
     $email = $_POST["email"];
@@ -12,18 +25,38 @@
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
     $chashed_password = password_hash($cpassword, PASSWORD_DEFAULT);
 
-    if(isset($_POST["submit"])){
-        if($password==$cpassword){
-            $_SESSION["status"] = 'Password match';
-           
-            
-        }
-        else{
-            $_SESSION["status"] = 'Password do not match';
-           
+    $validpattern = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^";  
+    if (!preg_match ($validpattern, $email) ){  
+        $ErrMsg = "Email is not valid. <br>";  
+                echo $ErrMsg;  
+    } else {  
+        echo "Your valid email address is: " .$email;
+    echo "<br>"; 
+    }  
+     
+
+    
+    if (isset($_POST["submit"])) {
+        if ($password == $cpassword) {
+
+            // //data to insert into table
+            // //$sql = "INSERT INTO `information`(`Full Name`, `User Name`, `Email`, `Phone`, `Password`, `Confirm Password`) VALUES ('$name','$username','$email','$phone','$hashed_password','$chashed_password')";
+    
+            // $result = mysqli_query($conn, $sql);
+            // //SQL status
+            // if ($result) {
+            //     echo "inserted <br>";
+            // } 
+
+            echo "matched";
+        } else {
+            echo "do not match <br>";
+            echo "not inserted".mysqli_error($conn);
         }
     }
 
+
+    
     // if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //     echo "Full Name: " . $name;
     //     echo "<br>";
@@ -40,38 +73,4 @@
     //     echo "Gender: " .$_POST["gender"];
     //     echo "<br>";
     // }
-
-    
-
-    // $servername = "localhost";
-    // $username = "root";
-    // $password = "";
-    // $database = "project2";
-
-    // //create a connection
-    
-    // $conn = mysqli_connect($servername, $username, $password, $database);
-
-    // //connection status
-    // if (!$conn) {
-    //     die("Disconnect" . mysqli_connect_error());
-    // } 
-    // else {
-    //     echo "Connected";
-    // }
-
-    // //data to insert into table
-    // //$sql = "INSERT INTO `information`(`Full Name`, `User Name`, `Email`, `Phone`, `Password`, `Confirm Password`) VALUES ('$name','$username','$email','$phone','$hashed_password','$chashed_password')";
-
-    // $result = mysqli_query($conn, $sql);
-    // //SQL status
-    // if ($result) {
-    //     echo "inserted <br>";
-    // } 
-    // else {
-    //     echo "not inserted".mysqli_error($conn);
-        
-    // }
     ?>
-</body>
-</html>
